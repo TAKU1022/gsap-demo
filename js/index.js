af02aaabc712e88a74c312e8fe054caf009f1f25
Takumi <nase49946@gmail.com>
2022-01-27 10:31:12 +0900

    timeline

--- js/index.js
+++ js/index.js
@@ -1,2 +1,6 @@
-console.log("check");
-console.log("ok");
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+let timeline = gsap.timeline();
+timeline.from(".box1", { duration: 2, scale: 0 });
