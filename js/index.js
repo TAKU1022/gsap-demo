5b7667cccb771aa8581af920c6fe1566a9d946e9
Takumi <nase49946@gmail.com>
2022-01-28 16:02:52 +0900

    スクロール

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
-let timeline = gsap.timeline();
-timeline.from(".box1", { duration: 2, scale: 0 });
+const targetList = [...document.querySelectorAll('[data-animation]')];
+
+window.addEventListener('load', () => {
+  targetList.forEach((target) => {
+    gsap.from(target, {
+      scrollTrigger: { trigger: target, markers: true, once: true },
+      duration: 2,
+      scale: 0,
+    });
+  });
+});
